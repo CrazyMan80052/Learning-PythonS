ed64ff77660bc7a0b1b75bc57ca882f831d6430a
FirstCommit <crazyman8005@gmail.com>
2019-10-19 14:58:00 -0400

    Homework 10/19/19

---
+++ org/talon/learning/Chapter 3,4,5,6/Chapter 3.py
@@ -0,0 +1,13 @@
+#1 Favorites
+games=["gaming", "watching", "homework"]
+foods=["ice cream", "chicken", "gulab jamun"]
+favorites=games+foods
+print(favorites)
+
+#2 Counting Combatants
+print(3*25+2*40)
+
+#3 Greetings
+firstname="Sahas"
+lastname="Uppalapti"
+print("Hi there,%s" %firstname+" "+lastname)
